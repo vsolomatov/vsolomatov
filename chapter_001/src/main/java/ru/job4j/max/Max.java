476ed3d4b8970ab6801c231f95a2df03b14b4aa4
Vyacheslav Solomatov <solomatoff.vyacheslav@yandex.ru>
2018-02-15 17:28:40 +0500

    3.1. Глупый бот[#41429]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,12 @@
+package ru.job4j.max;
+/**
+ * @author Viacheslav Solomatov (solomatoff.vyacheslav@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class Max {
+    public int max(int first, int second) {
+        return (first > second) ? first : second;
+    }
+}
