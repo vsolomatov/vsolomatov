c5c0be1966e332670b1f54ef08066c29defe328c
Vyacheslav Solomatov <solomatoff.vyacheslav@yandex.ru>
2018-02-18 10:40:41 +0500

    4.5. Рефакторинг кода[#41562]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,23 +1,42 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
+
 /**
  * @author Viacheslav Solomatov (solomatoff.vyacheslav@yandex.ru)
  * @version $Id$
  * @since 0.1
  */
 public class Paint {
-    /**
-     * Метод формирует строку в виде псевдографики пирамиды.
-     *
-     * @param height Высота пирамиды
-     * @return Строка (псевдографика пирамиды)
-     */
+    public String rightTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= column
+        );
+    }
+
+    public String leftTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= height - column - 1
+        );
+    }
+
     public String pyramid(int height) {
+        return this.loopBy(
+                height,
+                2 * height - 1,
+                (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+        );
+    }
+
+    private String loopBy(int height, int weight, BiPredicate<Integer, Integer> predict) {
         StringBuilder screen = new StringBuilder();
-        int weight = 2 * height - 1;
         for (int row = 0; row != height; row++) {
             for (int column = 0; column != weight; column++) {
-                if ( (row >= height - column - 1) && (row + height - 1 >= column) ) {
+                if (predict.test(row, column)) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
