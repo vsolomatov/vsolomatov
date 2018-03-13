a09239a7f3a9b3b7e4909765af4c10d98291f469
Vyacheslav Solomatov <solomatoff.vyacheslav@yandex.ru>
2018-03-13 22:54:15 +0500

    2. Очередь с приоритетом на LinkedList[#44897]

---
+++ chapter_002/src/main/java/ru/job4j/search/Task.java
@@ -0,0 +1,23 @@
+package ru.job4j.search;
+
+/**
+ * Класс описывает Задачу, имеющую строкоое описание
+ * и целочисленное поле приоритет.
+ */
+public class Task {
+    private String desc;
+    private int priority;
+
+    public Task(String desc, int priority) {
+        this.desc = desc;
+        this.priority = priority;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
