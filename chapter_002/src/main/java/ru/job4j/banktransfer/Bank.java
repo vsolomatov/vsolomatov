b294ffba1c96eba3a2493218b974e39c7c5d4b98
Vyacheslav Solomatov <solomatoff.vyacheslav@yandex.ru>
2018-03-19 16:58:25 +0500

    Банковские переводы[#45777]

---
+++ chapter_002/src/main/java/ru/job4j/banktransfer/Bank.java
@@ -0,0 +1,93 @@
+package ru.job4j.banktransfer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.TreeMap;
+
+/**
+ *  Класс для управления банковскими переводами
+ */
+public class Bank {
+
+    private TreeMap<User, ArrayList<Account>> treemap = new TreeMap<>();
+
+    /**
+     * Метод возвращает текущую карту для использования.
+     * @return this.treemap.
+     */
+    public TreeMap<User, ArrayList<Account>> getTreemap() {
+        return this.treemap;
+    }
+
+    /**
+     * Метод добавляет пользователя в нашу карту.
+     * @param user новый пользователь.
+     */
+    public void addUser(User user) {
+        this.treemap.putIfAbsent(user, new ArrayList<>());
+    }
+
+    /**
+     * Метод удаляет пользователя из нашей карты.
+     * @param user удаляемый пользователь.
+     */
+    public void deleteUser(User user) {
+        this.treemap.remove(user);
+    }
+
+    /**
+     * Метод добавляет новый счет указанному пользователю.
+     * @param user пользователь.
+     * @param account новый счет.
+     */
+    public void addAccountToUser(User user, Account account) {
+        this.treemap.get(user).add(account);
+    }
+
+    /**
+     * Метод возвращает конкретный счет указанного пользователя.
+     * @param user пользователь.
+     * @param account новый счет.
+     * @return счет пользователя.
+     */
+    private Account getActualAccount(User user, Account account) {
+        ArrayList<Account> list = this.treemap.get(user);
+        return list.get(list.indexOf(account));
+    }
+
+    /**
+     * Метод удаляет указанный счет указанного пользователя.
+     * @param user пользователь.
+     * @param account новый счет.
+     */
+    public void deleteAccountFromUser(User user, Account account) {
+        this.treemap.get(user).remove(account);
+    }
+
+    /**
+     * Метод возвращает список счетов указанного пользователя.
+     * @param user пользователь.
+     * @return список счетов пользователя.
+     */
+    public List<Account> getUserAccounts(User user) {
+        return this.treemap.get(user);
+    }
+
+    /**
+     * Метод переводит деньги с указанного счета пользователя на конкретный счет другого пользователя.
+     * @param user1 первый пользователь.
+     * @param account1 счет первого пользователя.
+     * @param user2 второй пользователь.
+     * @param account2 счет второго пользователя.
+     * @return если счёт не найден или не хватает денег на счёте (с которого переводятся деньги) должен вернуть false, в случае успеха - true.
+     */
+    public boolean transferMoney(User user1, Account account1,
+                            User user2, Account account2, double amount) {
+        return this.treemap.get(user1).contains(account1)
+                && this.treemap.get(user2).contains(account2)
+                && getActualAccount(user1, account1).transfer(
+                getActualAccount(user2, account2), amount);
+    }
+
+}
---
+++ chapter_002/src/main/java/ru/job4j/banktransfer/User.java
@@ -0,0 +1,51 @@
+package ru.job4j.banktransfer;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+public class User implements Comparable<User> {
+    private String name;
+    private String passport;
+
+    User(String name, String passport) {
+        this.name = name;
+        this.passport = passport;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int compareTo(@NotNull User o) {
+        return (this.name.equals(o.name)) ? this.passport.compareTo(o.passport) : this.name.compareTo(o.name);
+    }
+
+    public String toString() {
+        return "User " + this.hashCode() + " (Name=" + this.name + "; Passport=" + this.passport + ")";
+    }
+
+    /**
+     * Метод сравнения
+     * @param obj объект сравнения
+     * @return true - если объекты равны, иначе false
+     */
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true; //ссылки идентичные, сравнение не требуется
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        User user = (User) obj;
+        return user.name.equals(this.name) && user.passport.equals(this.passport); //сравниваем внутренности
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, passport);
+    }
+
+}
