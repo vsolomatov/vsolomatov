0d450b54ebe2ac539eb8a97b909493f930a02276
Vyacheslav Solomatov <solomatoff.vyacheslav@yandex.ru>
2018-03-29 07:43:55 +0500

    2. Set реализованный на связном списке[#47294]

---
+++ chapter_000/src/main/java/com/solomatoff/set/SimpleLinkedSet.java
@@ -0,0 +1,35 @@
+package com.solomatoff.set;
+
+import com.solomatoff.list.SimpleLinked;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+public class SimpleLinkedSet<E> implements Iterable<E> {
+    private SimpleLinked<E> container = new SimpleLinked<>();
+
+    public boolean add(E e) {
+        boolean result = true;
+        boolean exists = false;
+        // проверить на совпадение с уже имеющимися элементами коллекции
+        for (Iterator<E> i = container.iterator(); i.hasNext();) {
+            if (i.next().equals(e)) {
+                exists = true;
+                break;
+            }
+        }
+        if (exists) {
+            result = false;
+        } else {
+            container.add(e);
+        }
+        return result;
+    }
+
+    @NotNull
+    @Override
+    public Iterator<E> iterator() throws ConcurrentModificationException {
+        return container.iterator();
+    }
+}
