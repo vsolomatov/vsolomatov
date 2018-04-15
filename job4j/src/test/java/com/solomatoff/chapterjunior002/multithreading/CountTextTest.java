d9d5b8e5f4227f1da9e535e59fed51ab0d92db42
Vyacheslav Solomatov <solomatoff.vyacheslav@yandex.ru>
2018-04-15 10:55:13 +0500

    2. Сделать ожидание вывода[#49330]

--- job4j/src/test/java/com/solomatoff/chapterjunior002/multithreading/CountTextTest.java
+++ job4j/src/test/java/com/solomatoff/chapterjunior002/multithreading/CountTextTest.java
@@ -6,16 +6,13 @@ public class CountTextTest {
     @Test
     public void whenCountText() {
         String text = "Создать программу, которая будет считать количество слов и пробелов в тексте. Здесь не надо использовать регулярные выражения. Просто в цикле перебрать символы.";
-
         System.out.println("Начало расчетов");
         CountSpaces countSpaces = new CountSpaces(text);
         Thread t1 = new Thread(countSpaces, "thread 1");
         t1.start();
-
         CountWords countWords = new CountWords(text);
         Thread t2 = new Thread(countWords, "thread 2");
         t2.start();
-
         try {
             t1.join();
             t2.join();
