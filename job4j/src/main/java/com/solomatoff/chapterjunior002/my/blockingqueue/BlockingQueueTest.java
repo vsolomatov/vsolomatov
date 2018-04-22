736a635fdab7f571f17e07ddc081e47adf63305b
Vyacheslav Solomatov <solomatoff.vyacheslav@yandex.ru>
2018-04-22 11:35:05 +0500

    Мои примеры

--- job4j/src/main/java/com/solomatoff/chapterjunior002/my/blockingqueue/BlockingQueueTest.java
+++ job4j/src/main/java/com/solomatoff/chapterjunior002/my/blockingqueue/BlockingQueueTest.java
@@ -8,10 +8,8 @@ import java.util.concurrent.*;
  * @version 1.01 2012-01-26
  * @author Cay Horstmann
  */
-public class BlockingQueueTest
-{
-    public static void main(String[] args)
-    {
+public class BlockingQueueTest {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter base directory (e.g. /usr/local/jdk1.6.0/src): ");
         String directory = in.nextLine();
@@ -34,9 +32,8 @@ public class BlockingQueueTest
 /**
  * This task enumerates all files in a directory and its subdirectories.
  */
-class FileEnumerationTask implements Runnable
-{
-    public static File DUMMY = new File("");
+class FileEnumerationTask implements Runnable {
+    public static final File DUMMY = new File("");
     private BlockingQueue<File> queue;
     private File startingDirectory;
 
@@ -45,21 +42,16 @@ class FileEnumerationTask implements Runnable
      * @param queue the blocking queue to which the enumerated files are added
      * @param startingDirectory the directory in which to start the enumeration
      */
-    public FileEnumerationTask(BlockingQueue<File> queue, File startingDirectory)
-    {
+    public FileEnumerationTask(BlockingQueue<File> queue, File startingDirectory) {
         this.queue = queue;
         this.startingDirectory = startingDirectory;
     }
 
-    public void run()
-    {
-        try
-        {
+    public void run() {
+        try {
             enumerate(startingDirectory);
             queue.put(DUMMY);
-        }
-        catch (InterruptedException e)
-        {
+        } catch (InterruptedException e) {
         }
     }
 
@@ -67,13 +59,14 @@ class FileEnumerationTask implements Runnable
      * Recursively enumerates all files in a given directory and its subdirectories.
      * @param directory the directory in which to start
      */
-    public void enumerate(File directory) throws InterruptedException
-    {
+    public void enumerate(File directory) throws InterruptedException {
         File[] files = directory.listFiles();
-        for (File file : files)
-        {
-            if (file.isDirectory()) enumerate(file);
-            else queue.put(file);
+        for (File file : files) {
+            if (file.isDirectory()) {
+                enumerate(file);
+            } else {
+                queue.put(file);
+            }
         }
     }
 }
@@ -81,8 +74,7 @@ class FileEnumerationTask implements Runnable
 /**
  * This task searches files for a given keyword.
  */
-class SearchTask implements Runnable
-{
+class SearchTask implements Runnable {
     private BlockingQueue<File> queue;
     private String keyword;
 
@@ -91,34 +83,26 @@ class SearchTask implements Runnable
      * @param queue the queue from which to take files
      * @param keyword the keyword to look for
      */
-    public SearchTask(BlockingQueue<File> queue, String keyword)
-    {
+    public SearchTask(BlockingQueue<File> queue, String keyword) {
         this.queue = queue;
         this.keyword = keyword;
     }
 
-    public void run()
-    {
-        try
-        {
+    public void run() {
+        try {
             boolean done = false;
-            while (!done)
-            {
+            while (!done) {
                 File file = queue.take();
-                if (file == FileEnumerationTask.DUMMY)
-                {
+                if (file == FileEnumerationTask.DUMMY) {
                     queue.put(file);
                     done = true;
+                } else {
+                    search(file);
                 }
-                else search(file);
             }
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
-        }
-        catch (InterruptedException e)
-        {
+        } catch (InterruptedException e) {
         }
     }
 
@@ -126,13 +110,10 @@ class SearchTask implements Runnable
      * Searches a file for a given keyword and prints all matching lines.
      * @param file the file to search
      */
-    public void search(File file) throws IOException
-    {
-        try (Scanner in = new Scanner(file))
-        {
+    public void search(File file) throws IOException {
+        try (Scanner in = new Scanner(file)) {
             int lineNumber = 0;
-            while (in.hasNextLine())
-            {
+            while (in.hasNextLine()) {
                 lineNumber++;
                 String line = in.nextLine();
                 if (line.contains(keyword)) {
