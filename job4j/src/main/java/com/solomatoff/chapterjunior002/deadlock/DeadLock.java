f73daf505ebb7cae63d2fa3fd1f1614e8580c39a
Vyacheslav Solomatov <solomatoff.vyacheslav@yandex.ru>
2018-04-27 16:12:52 +0500

    Мои примеры

--- job4j/src/main/java/com/solomatoff/chapterjunior002/deadlock/DeadLock.java
+++ job4j/src/main/java/com/solomatoff/chapterjunior002/deadlock/DeadLock.java
@@ -28,10 +28,14 @@ public class DeadLock {
         final Friend gaston =
                 new Friend("Gaston");
         new Thread(new Runnable() {
-            public void run() { alphonse.bow(gaston); }
+            public void run() {
+                alphonse.bow(gaston);
+            }
         }).start();
         new Thread(new Runnable() {
-            public void run() { gaston.bow(alphonse); }
+            public void run() {
+                gaston.bow(alphonse);
+            }
         }).start();
     }
 }
